fbdc8579add8631a141dc71c36b093c4523cb36a
ghufron <ghufron@minatalink.local>
2021-09-14 05:37:39 +0700

    Add Style className

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,24 @@
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
 import Head from 'next/head'
 import React from 'react'
+import useStyles from '../utils/styles'
 
 export default function Layout({children}) {
+    const classes = useStyles()
     return (
         <div>
             <Head>
                 <title>Next Projet</title>
             </Head>
-            <AppBar position="static">
+            <AppBar position="static" className={classes.navbar}>
                 <Toolbar>
                     <Typography>Next Project</Typography>
                 </Toolbar>
             </AppBar>
-            <Container>
+            <Container className={classes.main}>
                 {children}
             </Container>
-            <footer>
+            <footer className={classes.footer}>
                 <Typography>All rights reserved. Next Project.</Typography>
             </footer>
         </div>
