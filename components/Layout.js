a73e2490bbdc28f38bf3057072f1e063430d28ce
ghufron <ghufron@minatalink.local>
2021-09-15 05:31:00 +0700

    try make themes

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,47 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import { createTheme, AppBar, Container, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
+import { green } from '@material-ui/core/colors'
 import Head from 'next/head'
 import React from 'react'
 import useStyles from '../utils/styles'
 
+
+
 export default function Layout({children}) {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: green[500],
+            },
+            secondary: {
+                main: '#f44336',
+            },
+        },
+    })
+    
     const classes = useStyles()
     return (
         <div>
             <Head>
                 <title>Next Projet</title>
             </Head>
-            <AppBar position="static" className={classes.navbar}>
+
+            <ThemeProvider theme={theme}>
+                <AppBar position="static" className={classes.navbar}>
                 <Toolbar>
                     <Typography>Next Project</Typography>
                 </Toolbar>
-            </AppBar>
-            <Container className={classes.main}>
-                {children}
-            </Container>
-            <footer className={classes.footer}>
-                <Typography>All rights reserved. Next Project.</Typography>
-            </footer>
+                </AppBar>
+
+                <Container className={classes.main}>
+                    {children}
+                </Container>
+
+                <footer className={classes.footer}>
+                    <Typography>All rights reserved. Next Project.</Typography>
+                </footer> 
+            </ThemeProvider>
+
+            
         </div>
     )
 }
