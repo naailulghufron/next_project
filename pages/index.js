3ad7c2f865cb560f50c0fb44a73e107d24d99719
naailulghufron <naailul@yahoo.com>
2021-09-14 14:42:25 +0700

    Try using form Material UI

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import { makeStyles, TextField } from '@material-ui/core'
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
 export default function Home() {
+  const classes = useStyles();
   return (
     <Layout>
       <div>
@@ -15,6 +26,12 @@ export default function Home() {
           <li>Item 4</li>
           <li>Item 5</li>
         </ul>
+
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="standard-basic" label="Standard" />
+          <TextField id="filled-basic" label="Filled" variant="filled" />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </form>
       </div>
     </Layout>
   )
